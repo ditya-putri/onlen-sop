655d6a5948b15bd8fba26a108b5dc99846903594
farhanline <farhan5307f@gmail.com>
2026-08-04 11:36:03 +0700

    first commit

--- vue-GUIMultimedia/src/pages/LandingPage.vue
+++ vue-GUIMultimedia/src/pages/LandingPage.vue
@@ -3,6 +3,9 @@
     <div class ="main">
     <h1>Selamat Datang di Onlineshop Sekolah</h1>
     <p>Belanja gampang, sekolah senang.</p>
+    <button class="btn-grad">
+      <router-link to="/product">Lihat Produk</router-link>
+    </button>
     </div>
   </main>
 </template>
--- vue-GUIMultimedia/src/pages/ProductPage.vue
+++ vue-GUIMultimedia/src/pages/ProductPage.vue
@@ -2,12 +2,12 @@
 import ProductCard from '../components/ProductCard.vue'
  
 const daftarProduk = [
-  { id: 1, nama: 'Kaos Sekolah', harga: 75000, gambar: 
-'https://placehold.co/150' },
-  { id: 2, nama: 'Topi Sekolah', harga: 35000, gambar: 
-'https://placehold.co/150' },
-  { id: 3, nama: 'Tas Sekolah', harga: 120000, gambar: 
-'https://placehold.co/150' },
+  { id: 1, nama: 'seblak', harga: 5000000, gambar: 
+'https://cdn.idntimes.com/content-images/community/2021/02/fromandroid-2a60232ed16b5f37d2f27e1af00ed96f.jpg' },
+  { id: 2, nama: 'bakso', harga: 10000000, gambar: 
+'https://cdn.idntimes.com/content-images/community/2019/06/774b2584-2da7-429e-b3aa-8a4a4561b974-225f13c2e62b17ee20e26d2b520f33fb.jpeg' },
+  { id: 3, nama: 'cilok', harga: 5000000, gambar: 
+'https://palpos.disway.id/upload/6e68ca59e89194824b3aa7dc598da23a.jpg' },
 ]
 </script>
  
